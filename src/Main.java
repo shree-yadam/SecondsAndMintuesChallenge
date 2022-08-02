b3726c899b3fc5c7adee52b3755e449f8cec3f80
Shree Devi Yadam <shree.yadam@gmail.com>
2022-08-01 21:44:32 -0700

    Create methods to calculate hours minutes and seconds

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static String getDurationString(int minutes, int seconds) {
+        if(minutes < 0 || seconds < 0 || seconds > 59) {
+            return "Invalid value";
+        }
+        int hours = minutes / 60;
+        minutes %= 60;
+        String hoursString = (hours/10 >= 1? hours : "0" + hours) + "h";
+        String minutesString = (minutes/10 >= 1? minutes : "0" + minutes) + "m";
+        String secondsString = (seconds/10 >= 1? seconds : "0" + seconds) + "s";
+        return hoursString + " " + minutesString + " " + secondsString;
+    }
+
+    public static String getDurationString(int seconds) {
+        if(seconds < 0) {
+            return "Invalid value";
+        }
+        int minutes = seconds / 60;
+        seconds %= 60;
+        return getDurationString(minutes, seconds);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDurationString(61, 0));
+        System.out.println(getDurationString(61 * 60));
+    }
+}
